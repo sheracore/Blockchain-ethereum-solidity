e668667cf500758f6f7cee8c12daf6cb37a6a524
Mohammad Ghaffari <34317945+sheracore@users.noreply.github.com>
2024-05-26 20:22:11 +0330

    Update sendMoneyExample.sol

--- sendMoneyExample.sol
+++ sendMoneyExample.sol
@@ -1,1 +1,24 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.14;
+
+contract WalletContract { 
+
+    function getContractBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function deposit() public payable{
+        payable(msg.sender).transfer(msg.value);
+
+    }
+
+    function withdrawAll() public{
+        payable(msg.sender).transfer(getContractBalance());
+    }
+
+    function withdrawToAddress(address payable to) public payable {
+        to.transfer(getContractBalance());
+    }
+  
+}
